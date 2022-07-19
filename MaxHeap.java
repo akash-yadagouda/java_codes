963a7e5655ae723fe112ef72e9e3c405c5ce89a7
akash-yadagouda <akashyadagoud@gmail.com>
2022-07-19 21:26:27 +0530

    stated with min heap

---
+++ MaxHeap.java
@@ -0,0 +1,13 @@
+class MaxHeap {
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+	public static void insert(int a, int[] arr, int n) {
+		//System.out.println(length);
+
+	}
+
+	public static void pop(int[] arr ) {
+
+	}
+}
